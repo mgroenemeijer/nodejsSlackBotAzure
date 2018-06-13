b5fb22a611dd2599ca39e8f7a76e3aa9b926142a
mgroenemeijer <mgroenemeijer@gmail.com>
2018-06-13 21:53:34 +0200

    Update helloWorldAzure.js

--- helloWorldAzure.js
+++ helloWorldAzure.js
@@ -1,6 +1,21 @@
-var http = require('http');
+/*var http = require('http');
 var port = process.env.port || 5000;
 http.createServer(function (req, res) {
     res.writeHead(200, { 'Content-Type': 'text/plain' });
     res.end('Hello W8rld\n');
 }).listen(port);
+*/
+
+var http = require('http');
+
+var server = http.createServer(function(request, response) {
+
+    response.writeHead(200, {"Content-Type": "text/plain"});
+    response.end("Hello World!");
+
+});
+
+var port = process.env.PORT || 1337;
+server.listen(port);
+
+console.log("Server running at http://localhost:%d", port);
