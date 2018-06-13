8d652f140f1659ba1cae6a2ad4df03cce31b3d9d
Mr. Green <chiel@Michiels-MacBook-Pro.local>
2018-06-13 22:28:04 +0200

    wijziging

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ var server = http.createServer(function(request, response) {
 
     response.writeHead(200, {"Content-Type": "text/plain"});
     response.end("Hello W9rld!");
+    
+	joep();
 
 });
 
@@ -11,3 +13,40 @@ var port = process.env.PORT || 1337;
 server.listen(port);
 
 console.log("Server running at http://localhost:%d", port);
+
+
+function joep(){
+
+
+    
+const { IncomingWebhook, WebClient } = require('@slack/client');
+
+console.log('Getting started with Slack Developer Kit for Node.js');
+
+const web = new WebClient(process.env.SLACK_TOKEN);
+const timeNotification = new IncomingWebhook(process.env.SLACK_WEBHOOK_URL);
+const currentTime = new Date().toTimeString();
+
+timeNotification.send(`The current time is ${currentTime}`, (error, resp) => {
+  if (error) {
+    return console.error(error);
+  }
+  const zoekTerm = "nederland";
+  console.log('Notification sent');
+  console.log('Waiting a few seconds for search indexes to update...');
+  setTimeout(() => {
+    console.log('Calling search.messages');
+    web.search.messages({ query: zoekTerm })
+      .then(resp => {
+        if (resp.messages.total > 0) {
+          console.log('First match:', resp.messages.matches[0]);
+        } else {
+          console.log('No matches found');
+        }
+      })
+      .catch(console.error)
+  }, 10);
+});
+
+
+}
